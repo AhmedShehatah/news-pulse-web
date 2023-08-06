93091bcd20d9f36823cfda92669a52922099ab20
Ahmed Hamdy <ah.hamd221@gmail.com>
2023-08-06 23:37:15 +0300

    feat: start in home page &&  responsive

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const menuIcon = document.getElementById("menu-icon");
+const arrowIcon = document.getElementById("arrow");
+menuIcon.addEventListener("click", () => {
+  const menuBox = document.getElementById("menu");
+  const navMenu = document.getElementById("left");
+  const overlay = document.getElementById("overlay");
+  navMenu.style.visibility = "hidden";
+  menuBox.style.left = 0;
+  overlay.style.display = "block";
+});
+arrowIcon.addEventListener("click", () => {
+  const menuBox = document.getElementById("menu");
+  const navMenu = document.getElementById("left");
+  const overlay = document.getElementById("overlay");
+  navMenu.style.visibility = "visible";
+  menuBox.style.left = `-300px`;
+  overlay.style.display = "none";
+});
+document.addEventListener("click", (e) => {
+  if (e.target.id == "overlay") {
+    const menuBox = document.getElementById("menu");
+    const navMenu = document.getElementById("left");
+    const overlay = document.getElementById("overlay");
+    navMenu.style.visibility = "visible";
+    menuBox.style.left = `-300px`;
+    overlay.style.display = "none";
+  }
+});
