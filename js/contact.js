cc9d0f257ebe0e83b82c2f59f749d776da41b6f8
Ahmed Hamdy <ah.hamd221@gmail.com>
2023-08-16 21:52:16 +0300

    feat: add about us

--- js/contact.js
+++ js/contact.js
@@ -5,21 +5,23 @@ function SubmitClicked() {
   const msg = document.getElementById("msg").value;
   const email = document.getElementById("email-input").value;
   const params = {
-    email:email,
-    report:msg
-  }
-  axios.post(`${URL}/api/v1/contact`, params)
-  .then(response => {
-    alert("sent successfully");
-    signInButton.style.setProperty("pointer-events", "all");
-    document.querySelector(".loading").style.setProperty("display", "none");
-    document.getElementById("msg").value = ""
-    document.getElementById("email-input").value = ""
-  }).catch(error => {
-    alert("try again");
-    signInButton.style.setProperty("pointer-events", "all");
-    document.querySelector(".loading").style.setProperty("display", "none");
-  });
+    email: email,
+    report: msg,
+  };
+  axios
+    .post(`${URL}/api/v1/contact`, params)
+    .then((response) => {
+      alert("sent successfully");
+      signInButton.style.setProperty("pointer-events", "all");
+      document.querySelector(".loading").style.setProperty("display", "none");
+      document.getElementById("msg").value = "";
+      document.getElementById("email-input").value = "";
+    })
+    .catch((error) => {
+      alert("try again");
+      signInButton.style.setProperty("pointer-events", "all");
+      document.querySelector(".loading").style.setProperty("display", "none");
+    });
 }
 function fillAllInputs() {
   const msg = document.getElementById("msg").value;
@@ -38,38 +40,38 @@ signInButton.addEventListener("click", (e) => {
   }
 });
 function logoutButtonClicked() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    showUI();
-  }
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+  showUI();
+}
 function showUI() {
-    const token = localStorage.getItem("token");
-    const logOut = document.getElementById("logged-out-UI");
-    const infoDiv = document.getElementById("info");
-  
-    if (token !== null) {
-      const user = JSON.parse(localStorage.getItem("user"));
-      const userName = user.user_name;
-      infoDiv.innerHTML = "";
-      let content = `
+  const token = localStorage.getItem("token");
+  const logOut = document.getElementById("logged-out-UI");
+  const infoDiv = document.getElementById("info");
+
+  if (token !== null) {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const userName = user.user_name;
+    infoDiv.innerHTML = "";
+    let content = `
       <b>${userName[0].toUpperCase()}</b>
       <span>${userName}</span>
       `;
-      infoDiv.innerHTML = content;
-      document
-        .getElementById("logged-in-UI")
-        .style.setProperty("display", "flex");
-      logOut.style.setProperty("display", "none");
-      document.getElementById("sgn-in").style.setProperty("display", "none");
-      document.getElementById("sgn-up").style.setProperty("display", "none");
-    } else {
-      document
-        .getElementById("logged-in-UI")
-        .style.setProperty("display", "none");
-      logOut.style.setProperty("display", "flex");
-      document.getElementById("sgn-in").style.setProperty("display", "block");
-      document.getElementById("sgn-up").style.setProperty("display", "block");
-    }
+    infoDiv.innerHTML = content;
+    document
+      .getElementById("logged-in-UI")
+      .style.setProperty("display", "flex");
+    logOut.style.setProperty("display", "none");
+    document.getElementById("sgn-in").style.setProperty("display", "none");
+    document.getElementById("sgn-up").style.setProperty("display", "none");
+  } else {
+    document
+      .getElementById("logged-in-UI")
+      .style.setProperty("display", "none");
+    logOut.style.setProperty("display", "flex");
+    document.getElementById("sgn-in").style.setProperty("display", "block");
+    document.getElementById("sgn-up").style.setProperty("display", "block");
   }
-  
-  showUI();
+}
+
+showUI();
